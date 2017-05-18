ec6aee658729ca3b6058e097c1b5abd19c63ff50
Davis Ansis <ansis.davis.grantins@accenture.com>
2017-05-18 09:33:59 +0300

    fixed 'undefined' problem

--- crawler_app.js
+++ crawler_app.js
@@ -10,36 +10,36 @@ let callOnce = true;
 let testCount = 0;
 const allLinks = {};
 let outputOnce = true;
-let searchDepth = 5;
+const searchDepth = 5;
+
 const parseBody = (urlPage, linkParent) => {
-    if (urlPage.substring(0, 1) == '/'){
-      request((startUrl + urlPage), (error, response, body) =>{
-        if (error) {
-            console.log("Error" + error);
-        }
-        if (response.statusCode === 200) {
-            var parsedHtml = cheerio.load(body);
-            findLinks(parsedHtml, urlPage, linkParent);
-        }
-    });
+    if (urlPage.substring(0, 1) == '/') {
+        request((startUrl + urlPage), (error, response, body) => {
+            if (error) {
+                console.log("Error" + error);
+            }
+            if (response.statusCode === 200) {
+                var parsedHtml = cheerio.load(body);
+                findLinks(parsedHtml, urlPage, linkParent);
+            }
+        });
     } else {
-        request((urlPage), (error, response, body) =>{
-        if (error) {
-            console.log("Error" + error);
-        }
-        if (response.statusCode === 200) {
-            var parsedHtml = cheerio.load(body);
-            findLinks(parsedHtml, urlPage, linkParent);
-        }
-    });
+        request((urlPage), (error, response, body) => {
+            if (error) {
+                console.log("Error" + error);
+            }
+            if (response.statusCode === 200) {
+                var parsedHtml = cheerio.load(body);
+                findLinks(parsedHtml, urlPage, linkParent);
+            }
+        });
     }
 };
 
-promptly.prompt('Please write website : ', function (err, value){
+promptly.prompt('Please write website : ', function (err, value) {
     startUrl = value;
     parseBody('/', '');
-    console.log(value);
-})
+});
 
 const findLinks = (parsedHtml, pageUrl, linkParent) => {
     testCount++;
@@ -50,36 +50,39 @@ const findLinks = (parsedHtml, pageUrl, linkParent) => {
         const eachLinkFound = [];
         links.each( function() {
             let inputLink = parsedHtml(this).attr('href');
-            console.log(inputLink);
-            if (inputLink.includes(startUrl) || inputLink.substring(0, 1)== '/' ) { //checks if they are routes
-                if (inputLink == pageUrl){ //checks if current url equal child url
-                } else if (inputLink in allLinks) { // checks if current url already exists
-                    let foundLinksParents = allLinks[inputLink].parent;
-                    if (!foundLinksParents.includes(linkParent)) {
-                        allLinks[inputLink]
-                            .parent
-                            .push(linkParent);
+            if (!(typeof inputLink === 'undefined')) { // checks if link is not equal to undefined
+                if (inputLink.includes(startUrl) || inputLink.substring(0, 1) == '/' ) { //checks if they are routes
+                    //checks if current url equal child url
+                    if (inputLink == pageUrl){ //checks if current url equal child url
+                    } else if (inputLink in allLinks) { // checks if current url already exists
+                        let foundLinksParents = allLinks[inputLink].parent;
+                        if (!foundLinksParents.includes(linkParent)) {
+                            allLinks[inputLink]
+                                .parent
+                                .push(linkParent);
+                        }
+                    } else {
+                        eachLinkFound.push(inputLink);
+                        parseBody(inputLink, pageUrl);
+                        allLinks[pageUrl] = {
+                            title: parsedTitle,
+                            child: eachLinkFound,
+                            parent: [linkParent],
+                            img: parsedImg
+                        };
                     }
-                } else {
-                    eachLinkFound.push(inputLink);
-                    parseBody(inputLink, pageUrl);
-                    allLinks[pageUrl] = {
-                        title: parsedTitle,
-                        child: eachLinkFound,
-                        parent: [linkParent],
-                        img: parsedImg
-                    };
                 }
             }
         });
     } else {
-                if (outputOnce === true) {
-                    fs.writeFile("crawlerResults.json", JSON.stringify(allLinks, null, '  '), function (err) {
-                        if (err) throw err;
-                    });
-                    console.log(JSON.stringify(allLinks, null, '  '));
-                    outputOnce = false;
-                }
+        if (outputOnce === true) {
+            fs.writeFile("crawlerResults.json", JSON.stringify(allLinks, null, '  '), function (err) {
+                if (err) throw err;
+            });
+            console.log(JSON.stringify(allLinks, null, '  '));
+            console.log('All is written is crawlerResults.json file');
+            outputOnce = false;
+        }
     }
 };
 
